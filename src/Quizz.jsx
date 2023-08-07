57f9723c54564f8d791955189ea4a73cffa0ccb2
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2023-08-07 17:45:13 -0400

    initial API testing

--- src/Quizz.jsx
+++ src/Quizz.jsx
@@ -1,5 +1,19 @@
+import React from "react"
+
 
 export default function Quizz() {
+
+    const [questions, setQuestions] = React.useState('');
+
+    React.useEffect( () => {
+        console.log('useEffect Ran')
+        fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+            .then( (res) => res.json() )
+            .then( (data) => setQuestions(data))
+    },[])
+
+    console.log(questions.results[0].question)
+
     return(
         <main>
             <h2>Quizz Placeholder</h2>
